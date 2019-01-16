9e9620b246348a2cebf208c65692a2efc3e171dd
zakariaharti <zakariya.developer@gmail.com>
2019-01-16 20:50:38 +0100

    init commit

---
+++ stories/index.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+const stories = storiesOf('Basic App', module);
+
+stories.add('Exapmle App',() => <h3>hellow world</h3>);
